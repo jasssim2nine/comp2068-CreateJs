301a74f39968cc8cfa048375bace7d3e3ddb946e
Jasim Khan <jasim.khan89@gmail.com>
2020-03-29 11:23:33 -0400

    created createjs site structure

---
+++ comp2068-createjs/Script/game.js
@@ -0,0 +1,17 @@
+var canvas;
+var stage;
+function init() {
+    canvas = document.getElementById("canvas");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", gameLoop);
+    main();
+}
+function gameLoop() {
+    stage.update();
+}
+function main() {
+    var helloText = new createjs.Text("Hello World", "40px Consolas", "#000000");
+    stage.addChild(helloText);
+}
+//# sourceMappingURL=game.js.map
